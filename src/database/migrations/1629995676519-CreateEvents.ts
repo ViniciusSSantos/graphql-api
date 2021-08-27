008324a15fb5b154218463143ea91259c6059b3f
Vinicius Santos <55525181+ViniciusSSantos@users.noreply.github.com>
2021-08-27 09:31:21 -0300

    Create events migration

---
+++ src/database/migrations/1629995676519-CreateEvents.ts
@@ -0,0 +1,59 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class CreateEvents1629995676519 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name:"events",
+                columns:[
+                    {
+                        name: "id",
+                        type: "uuid",
+                        isPrimary: true,
+                      },
+                      {
+                        name: "created_by",
+                        type: "uuid",
+                      },
+                      {
+                        name: "description",
+                        type: "varchar",
+                      },
+                      {
+                        name: "start_time",
+                        type: "varchar",
+                      },
+                      {
+                        name: "end_time",
+                        type: "varchar",
+                      },                      
+                      {
+                        name: "start_date",
+                        type: "varchar",
+                      },
+                      {
+                        name: "end_date",
+                        type: "varchar",
+                      },
+                      {
+                        name: "created_at",
+                        type: "timestamp",
+                        default: "now()",
+                      },
+                      {
+                        name: "updated_at",
+                        type: "timestamp",
+                        default: "now()",
+                      },
+                ]
+
+            })
+        )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("events");
+    }
+
+}
