b538e9791e7be6100c8d4c53253f05db377f558c
Vinicius Santos <55525181+ViniciusSSantos@users.noreply.github.com>
2021-09-09 15:15:16 -0300

    login mutation

--- src/resolvers/UserResolver.ts
+++ src/resolvers/UserResolver.ts
@@ -1,48 +1,88 @@
-import { Mutation, Resolver, InputType, Field, Arg } from "type-graphql";
+import { isAuth } from './../middlewares/isAuth';
+import { MyContext } from './../MyContext';
+import { Mutation, Resolver, InputType, Field, Arg, Query, Ctx, UseMiddleware, ObjectType } from "type-graphql";
 import { BaseEntity } from "typeorm";
 import { User } from "../entities/User";
-import {hash} from "bcryptjs"
+import { compare, hash } from "bcryptjs"
+import { sign } from 'jsonwebtoken';
 
 @InputType()
-class UserInput{   
-    @Field()
-    name!: string;
+class UserInput {
+  @Field()
+  name!: string;
 
-    @Field()
-    email!: string;
+  @Field()
+  email!: string;
 
-    @Field()
-    password!: string;
+  @Field()
+  password!: string;
 }
 
 
-@Resolver()
-export class UserResolver extends BaseEntity{
-    @Mutation(() => User)
-    async createUser(
-        @Arg("input", () => UserInput) input: UserInput        
-        ){
-         /*    const UserAlreadyExists = await User.findOne({ where: {email: input.email}})  
 
-            if(UserAlreadyExists){
-              return {
-                  errors: [{
-                    field: "email",
-                    message: "User already exists"
-                  }]
-              }
-            } */
-            
-        const passwordHash = await hash(input.password, 8)
-        
-        const user = User.create({name: input.name, email: input.email, password: passwordHash})
-       
-        
-        return user;
-        
+@ObjectType()
+class LoginResponse {
+  @Field()
+  Token!: string;
+  @Field(() => User)
+  user!: User;
+}
+
+@Resolver()
+export class UserResolver extends BaseEntity {
+
+  @Mutation(() => User)
+  async createUser(
+    @Arg("input", () => UserInput) input: UserInput
+  ) {
+    const passwordHash = await hash(input.password, 8)
+    const user = User.create({
+      name: input.name,
+      email: input.email,
+      password: passwordHash
+    })
+
+    return user;
+  }
+
+  @Query(() => String)
+  @UseMiddleware(isAuth)
+  CheckMiddleware(@Ctx() { payload }: MyContext) {
+    console.log(payload);
+    return `your user id is: ${payload!.userId}`;
+  }
+
+  @Mutation(() => LoginResponse)
+  async login(
+    @Arg("email") email: string,
+    @Arg("password") password: string,
+    @Ctx() { res }: MyContext
+  ) {
+    const user = await User.findOne({ where: { email } })
+
+    if (!user) {
+      throw new Error("Email incorrect");;
     }
-    
-    
+
+    const passwordMatch = await compare(password, user.password);
+
+    if (!passwordMatch) {
+      throw new Error("Password incorrect");;
+    }
+
+    const token = sign(
+      {
+        email: user.email,
+      },
+      process.env.JWT_SECRET!,
+      {
+        subject: user.id,
+        expiresIn: "1d",
+      }
+    );
+
+    return { Token: token, user }
+  }
 }
 
 
